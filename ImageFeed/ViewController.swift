5892b35850f777c7b2796e1c8f54f16d70e0defe
Olga Kuimova <olga.kuimova.97@mail.ru>
2023-10-14 19:42:12 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Kuimova Olga on 14.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
