56c596cb2c8ac9f6d965e553b5c659f0fad51f24
Olgapawpaw <olga.kuimova.97@mail.ru>
2023-11-26 13:54:11 +0300

    Задание по спринту 9

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -3,15 +3,15 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     
-    // MARK: - IB Outlets
+// MARK: - IB Outlets
     @IBOutlet private var photoImage: UIImageView!
     @IBOutlet private var likeButton: UIButton!
     @IBOutlet private var dateLabel: UILabel!
 
-    // MARK: - Public Properties
+// MARK: - Public Properties
     static let reuseIdentifier = "ImagesListCell"
     
-    // MARK: - Private Properties
+// MARK: - Private Properties
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -19,7 +19,7 @@ final class ImagesListCell: UITableViewCell {
         return formatter
     }()
     
-    // MARK: - Public Methods
+// MARK: - Public Methods
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
         let currentPhotosName = photosName[indexPath.row]
         let likeActiveImage = UIImage(named: "LikeActive")
